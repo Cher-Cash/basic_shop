66d61514e28c8c317e97102644955b12e9656201
loki <gunlinux@ya.ru>
2024-09-10 21:59:07 +1000

    basic admin

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 from flask_migrate import Migrate
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
-
-migrate = Migrate()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SECRET_KEY'] = 'TypeMeIn'
 db = SQLAlchemy(app)
-migrate.init_app(app=app, db=db)
+migrate = Migrate(app, db)
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,16 +19,14 @@ class Product(db.Model):
     product = db.Column(db.String(50))
     description = db.Column(db.String(2048))
     discount = db.Column(db.Integer)
-    attributes = db.relationship('Attributes', backref='product', lazy=True)
+    attributes = db.relationship('Attributes', backref='product', lazy=False)
     orders = db.relationship('Orders', backref='product', lazy=True)
-
 
 class Attributes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
     value = db.Column(db.String(30))
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,9 +35,18 @@ class Orders(db.Model):
     status = db.Column(db.String(10))
     prod_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
+class MyModelView(ModelView):
+    column_display_all_relations = True
+    column_hide_backrefs = False
+
+admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
+admin.add_view(MyModelView(Product, db.session))
+admin.add_view(MyModelView(Attributes, db.session))
+admin.add_view(MyModelView(Orders, db.session))
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
