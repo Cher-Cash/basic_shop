18247032044db01b250487fce07c68c0a2eb86fc
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-09-21 17:53:00 +1000

    add product page with form for email

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_migrate import Migrate
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -60,6 +60,19 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/product/<int:product_id>', methods=["POST", "GET"])
+def product_detail(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method == "POST":
+        email = request.form.get('email')
+        new_order = Orders(email=email, status='pending', prod_id=product_id)
+        db.session.add(new_order)
+        db.session.commit()
+        flash('Ваш заказ успешно оформлен!', 'success')
+        return render_template('buing_page.html', product=product, preorder=True)
+    return render_template('buing_page.html', product=product, preorder=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
