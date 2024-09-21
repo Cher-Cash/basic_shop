caaa1e1e080ebc1ce995a69c1149e5deef9a6c1c
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-09-21 18:25:12 +1000

    add order page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_migrate import Migrate
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -61,6 +61,12 @@ def contact():
 def about():
     return render_template('about.html')
 
+
+@app.route('/order/<int:order_id>')
+def order(order_id):
+    order = Orders.query.get_or_404(order_id)
+    return render_template('order.html', order=order)
+
 @app.route('/product/<int:product_id>', methods=["POST", "GET"])
 def product_detail(product_id):
     product = Product.query.get_or_404(product_id)
@@ -69,9 +75,9 @@ def product_detail(product_id):
         new_order = Orders(email=email, status='pending', prod_id=product_id)
         db.session.add(new_order)
         db.session.commit()
-        flash('Ваш заказ успешно оформлен!', 'success')
-        return render_template('buing_page.html', product=product, preorder=True)
+        return redirect(url_for('order', order_id=new_order.id))
     return render_template('buing_page.html', product=product, preorder=False)
+
 
 
 if __name__ == '__main__':
