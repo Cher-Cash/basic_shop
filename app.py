d851efd056a72ef2892fd603f08a5304349f397f
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-09-21 16:20:44 +1000

    active page identification in nav

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ admin.add_view(MyModelView(Orders, db.session))
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
