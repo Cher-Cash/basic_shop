96114dd55460fdb4a100e58ba9182595e1718de0
Kovach Alexey <kovach.aleksey19@yandex.ru>
2024-09-21 15:08:53 +1000

    add page contact

--- app.py
+++ app.py
@@ -47,6 +47,9 @@ admin.add_view(MyModelView(Orders, db.session))
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
