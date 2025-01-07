358b4e7cb6d9642788a753634c55afaea0d9eed4
Cher-Cash <119042176+Cher-Cash@users.noreply.github.com>
2025-01-07 18:38:15 +1000

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def generate_signature(order_id, secret_key):
-    sigma = f"{order_id}{secret_key}"
+def generate_signature(order_id, company_id, secret_key):
+    sigma = f"{order_id}{company_id}{secret_key}"
     hash_object = hashlib.sha256(sigma.encode())
     signature = hash_object.hexdigest()
     return signature
